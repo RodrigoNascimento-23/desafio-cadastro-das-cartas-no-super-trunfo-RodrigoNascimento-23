7d0ca3b6a325d32e2b24801c0c05571c2b33c145
RodrigoNascimento-23 <rodrigo_eng@outlook.com.br>
2025-09-05 18:41:26 +0000

    Desafio Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main (){
     float Area,Area2;
     float PIB,PIB2;
     int Pontosturisticos,Pontosturisticos2;
+    float densidade1, densidade2;
+    float PIBpercap1,PIBpercap2;
 
 
     printf ("Informe o Estado: \n");
@@ -53,10 +55,24 @@ int main (){
     printf("Quantos pontos turisticos possuem a cidade de %s: \n", Cidade2);
     scanf("%d", &Pontosturisticos2);
 
-    printf("O código %s refere-se ao Estado de %s, já o código %s refere-se ao Estado de %s \n", Codigo,Estado,Codigo2,Estado2);
-    printf("A cidade escolhida na primeira opção foi %s, que possui uma população total de %d habitantes, com área aproximada de %fm² \n", Cidade,População,Area);
-    printf("Seguindo pelo código %s referente a segunda carta que corresponde ao Estado de %s, com uma população de %d habitantes e Área de %fm² aproximadamente \n", Codigo2, Estado2, População2, Area2);
-    printf("O PIB de %s é de aproximadamente %f, já o de %s é de %f aproximadamente \n",Estado, PIB,Estado2,PIB2);
-    printf("A cidade de %s possui %d pontos turisticos encantadores e %s possuem %d", Cidade,Pontosturisticos,Cidade2,Pontosturisticos2);
+    printf("->O código %s refere-se ao Estado de: %s \n", Codigo,Estado);
+    printf("->A cidade escolhida foi: %s\n->Possui uma população total de: %d habitantes \n->Com área aproximada de: %fkm² \n", Cidade,População,Area);
+    printf("->O PIB de %s é de aproximadamente %f \n",Estado, PIB);
+    printf("->A cidade de %s possui %d pontos turisticos \n", Cidade,Pontosturisticos);
+    densidade1 = População/Area;
+    PIBpercap1 = PIB/População;
+    printf("->A Densidade Populacional de %s é de: %f\n",Estado, densidade1);
+    printf("->O PIB Per Capita de %s é de: %f\n", Estado, PIBpercap1);
 
+    printf("\n");
+
+    printf("->O código %s refere-se ao Estado de %s \n", Codigo2,Estado2);
+    printf("->A cidade escolhida foi: %s\n->Possui uma população total de: %d habitantes \n->Com área aproximada de: %fkm² \n", Cidade2,População2,Area2);
+    printf("->O PIB de %s é de aproximadamente: %f \n",Estado2, PIB2);
+    printf("->A cidade de %s possui %d pontos turisticos \n", Cidade2,Pontosturisticos2);
+    densidade2 = População2/Area2;
+    PIBpercap2 = PIB2/População2;
+    printf("->A Densidade Populacional de %s é de: %f\n",Estado2, densidade2);
+    printf("->O PIB Per Capita de %s é de: %f\n", Estado2, PIBpercap2);
+    
     }
